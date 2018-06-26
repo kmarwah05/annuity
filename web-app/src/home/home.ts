fd2fe26383c25541224f7c775275284b5e080689
William Cabell <wcabell@pietech.com>
2018-06-26 16:11:21 -0400

    components working, bound inputs

--- web-app/src/home/home.ts
+++ web-app/src/home/home.ts
@@ -1,3 +1,21 @@
-export class Home {
+import { FundingSource, Sex, FilingStatus, Inputs } from "scripts/inputs";
 
+export class Home {
+  // Bring types into VM scope
+  private Sex: typeof Sex = Sex;
+  private FilingStatus: typeof FilingStatus = FilingStatus;
+  private FundingSource: typeof FundingSource = FundingSource;
+
+  inputs: Inputs;
+
+  constructor() {
+    this.inputs = new Inputs();
+  }
+
+  print(): void {
+    Object.keys(this.inputs).forEach(element => {
+      console.log(element);
+      console.log(this.inputs[element]);
+    });
+  }
 }
