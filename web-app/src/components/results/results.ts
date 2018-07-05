8435eef81c09cbe82f78e74510e334aeb244fb9e
William Cabell <wcabell@pietech.com>
2018-07-05 14:25:16 -0400

    Finished results page

--- web-app/src/components/results/results.ts
+++ web-app/src/components/results/results.ts
@@ -6,12 +6,12 @@ import { Inputs } from 'scripts/inputs';
 
 @inject(EventAggregator)
 export class Results {
-  chartMax: number = 15000;
-  
   @bindable inputs: Inputs;
   @bindable data: Data;
 
   ea: EventAggregator;
+  chart: Chart;
+  chartMax: number = 50000;
 
   get fixedPercentage() {
     return Math.round(this.data.brokerageBelowFixed * 100);
@@ -30,40 +30,57 @@ export class Results {
   }
 
   attached() {
-    // this.chartMax = this.data.brokerage[0].reduce((a, b) => a > b ? a : b);
+    this.ea.subscribe("reload", () => {
+      this.buildChart();
+    });
 
-    this.buildChart("Brokerage Account",
-      "brokerage-account-chart",
-      this.data.brokerage,
-      true);
-    this.buildChart("Variable Annuity",
-      "variable-annuity-chart",
-      this.data.variable,
-      false);
-    this.buildChart("Fixed Indexed Annuity",
-      "fixed-indexed-annuity-chart",
-      this.data.fixedIndexed,
-      false);
+    this.buildChart();
   }
 
   newInputs() {
     this.ea.publish("new inputs");
   }
 
-  buildChart(title: string,
-    target: string,
-    trials: number[][],
-    isFirst: boolean) {
-    let ctx = (document.getElementById(target) as HTMLCanvasElement).getContext("2d");
-    
-    let bars = trials.map(trial => trial.reduce((a, b) => a + b) / trial.length);
-      console.log(bars);
+  buildChart() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    let ctx = (document.getElementById("chart") as HTMLCanvasElement).getContext("2d");
+
+    let brokerageBars = this.data.brokerage.map(trial => trial.reduce((a, b) => a + b) / trial.length).sort((a, b) => a - b);
+    let variableBars = this.data.variable.map(trial => trial.reduce((a, b) => a + b) / trial.length).sort((a, b) => a - b);
+    let fixedIndexedBars = this.data.fixedIndexed.map(trial => trial.reduce((a, b) => a + b) / trial.length).sort((a, b) => a - b);
+
+    this.chartMax = Math.ceil(Math.max(...fixedIndexedBars) * 2 / 10000) * 10000;
+
+    let lowerQuartileIndex = Math.floor((brokerageBars.length - 1) * .25);
+    let upperQuartileIndex = Math.floor((brokerageBars.length - 1) * .75);
+
     let data = {
-      labels: bars.map((_, index) => String(index)),
+      labels: ["Worst Case", "Median Case", "Best Case"],
       datasets: [{
-        label: "Trials",
-        data: bars,
-        backgroundColor: "#2ecc71",
+        label: "Brokerage",
+        data: [brokerageBars[lowerQuartileIndex], this.data.brokerageMedian, brokerageBars[upperQuartileIndex]],
+        backgroundColor: "rgb(148, 124, 176)",
+        borderWidth: 0
+      },
+      {
+        label: "Variable",
+        data: [variableBars[lowerQuartileIndex], this.data.variableMedian, variableBars[upperQuartileIndex]],
+        backgroundColor: "rgb(89, 171, 227)",
+        borderWidth: 0
+      },
+      {
+        label: "Fixed Indexed",
+        data: [fixedIndexedBars[lowerQuartileIndex], this.data.fixedIndexedMedian, fixedIndexedBars[upperQuartileIndex]],
+        backgroundColor: "rgb(4, 147, 114)",
+        borderWidth: 0
+      },
+      {
+        label: "Fixed",
+        data: [this.data.fixed, this.data.fixed, this.data.fixed],
+        backgroundColor: "rgb(135, 211, 124)",
         borderWidth: 0
       }]
     };
@@ -73,52 +90,78 @@ export class Results {
         padding: 0
       },
       legend: {
-        display: false
+        position: "bottom",
+        labels: {
+          fontColor: "rgb(34, 34, 34)",
+          fontFamily: "'PT Serif', 'Times New Roman', Times, serif",
+          fontSize: 18,
+          padding: 20
+        }
+      },
+      tooltips: {
+        titleFontFamily: "'PT Sans', 'Helvetica Neue', Helvetica, sans-serif",
+        titleFontSize: 18,
+        bodyFontFamily: "'PT Serif', 'Times New Roman', Times, serif",
+        bodyFontSize: 18,
+        displayColors: false,
+        position: "average",
+        callbacks: {
+          label: function(tooltipItem, data) {
+              let label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+              if (label) {
+                  label += ': ';
+              }
+              label += "$" + tooltipItem.yLabel.format(2, 3);
+              return label;
+          }
+        }
       },
       title: {
         display: true,
-        text: title,
+        text: "Average yearly payout (USD)",
         fontColor: "rgb(34, 34, 34)",
         fontFamily: "'PT Sans', 'Helvetica Neue', Helvetica, sans-serif",
-        fontSize: 28
+        fontSize: 36,
+        padding: 30
       },
-      events: [],
-      animation: {
-        duration: 0
-      },
-      responsiveAnimationDuration: 0,
+      maintainAspectRatio: true,
       scales: {
         xAxes: [{
           gridLines: {
             display: false
           },
           ticks: {
-            display: false
+            display: true,
+            fontColor: "rgb(34, 34, 34)",
+            fontFamily: "'PT Serif', 'Times New Roman', Times, serif",
+            fontSize: 18,
+            padding: 10
           },
           barPercentage: 1.0,
-          categoryPercentage: 1.0
+          categoryPercentage: 0.7
         }],
         yAxes: [{
           scaleLabel: {
-            display: isFirst,
-            max: this.chartMax,
-            min: 0,
-            labelString: "Average yearly payout (USD)",
             fontColor: "rgb(34, 34, 34)",
             fontFamily: "'PT Serif', 'Times New Roman', Times, serif",
             fontSize: 18
           },
           ticks: {
-            display: isFirst,
+            display: true,
+            max: this.chartMax,
             fontColor: "rgb(34, 34, 34)",
             fontFamily: "'PT Serif', 'Times New Roman', Times, serif",
-            fontSize: 16
+            fontSize: 16,
+            callback: function(value) {
+              return '$' + value.format(2, 3);
+            }
           }
         }]
       }
     };
     
-    new Chart(ctx, {
+    this.chart = new Chart(ctx, {
       type: "bar",
       data: data,
       options: options
