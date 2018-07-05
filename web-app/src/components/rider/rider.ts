01239bbbfb2953afceb8f6396bf43d83653b7042
William Cabell <wcabell@pietech.com>
2018-07-05 10:04:47 -0400

    Finished linking up to back end

--- web-app/src/components/rider/rider.ts
+++ web-app/src/components/rider/rider.ts
@@ -1,9 +1,32 @@
-import { bindable } from "aurelia-framework";
+import { bindable, inject } from "aurelia-framework";
+import { EventAggregator } from "aurelia-event-aggregator";
 
+@inject(EventAggregator)
 export class Rider {
   @bindable rider: string;
+  @bindable riders: string[];
 
-  toggleRider(rider: string) {
-    document.getElementById(rider).classList.toggle("riders__checkbox--checked");
+  ea: EventAggregator;
+
+  constructor(EventAggregator) {
+    this.ea = EventAggregator;
+  }
+
+  attached() {
+    if (this.riders.includes(this.rider)) {
+      document.getElementById(this.rider).classList.add("riders__checkbox--checked");
+    }
+  }
+
+  toggleRider() {
+    document.getElementById(this.rider).classList.toggle("riders__checkbox--checked");
+
+    if (this.riders.includes(this.rider)) {
+      this.riders = this.riders.filter(entry => entry != this.rider);
+    } else {
+      this.riders.push(this.rider);
+    }
+
+    this.ea.publish("reload");
   }
 }
